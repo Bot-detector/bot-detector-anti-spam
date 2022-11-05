38c40075344e252be6c95ef036d757f6382aab18
extreme4all <40169115+extreme4all@users.noreply.github.com>
2022-11-05 23:10:14 +0100

    Update Token.java

--- src/main/java/com/botdetectorantispam/model/Token.java
+++ src/main/java/com/botdetectorantispam/model/Token.java
@@ -3,8 +3,8 @@ import com.botdetectorantispam.enums.Type;
 
 // {"viagra":Token(ham=100, spam=1000), "free":Token(ham=10, spam=100)}
 public class Token {
-    public int ham = 1;
-    public int spam = 1;
+    public int ham = 0;
+    public int spam = 0;
 
     public void add(int amount, Type dataType) {
         switch (dataType){
@@ -19,5 +19,10 @@ public class Token {
 
     public int total(){
         return ham + spam;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" + "ham=" + ham + ", spam=" + spam + '}';
     }
 }
